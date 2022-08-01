ff8e468a39286a295155cfbf9455507f4e2f9a53
muu <git968@choco.la>
2022-08-01 06:26:51 +0000

    add status colomn

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,15 +10,17 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</th>
                     <th>やること</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
+                    
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
-                    <td>{{ $task->check }}</td>
                 </tr>
                 @endforeach
             </tbody>
